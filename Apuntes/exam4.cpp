8404ee0aa3c00e75708140f142aa61cf114f7b10
SandovalAdrianUTEC <adrian.sandoval@utec.edu.pe>
2022-09-27 08:59:56 -0500

    new content

---
+++ Apuntes/exam4.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    // map tiene un key y un value
+    map<string, int> dict;
+    string word;
+
+    // contenedor ordenado y sin repeticion
+    set<string> stop_words = {"in", "at"};
+
+    // Imprime cada linea la cantidad
+    while(getline(cin, word)) {
+        // si la palabra no existe
+        if (word.begin() == word.end())
+            break;
+        // si la palabra no es un stop_word se cuenta
+        if (stop_words.find(word) == stop_words.end())
+            dict[word]++;
+    }
+    for (const auto& i: dict) {
+        cout << "(" << i.first << "," << i.second << ") ";
+    } cout << endl;
+
+    vector<int> numbers = {1,3,4,5,2,3,1,3};
+
+    // cuando se incializa set (del mismo tipo nativo) con los iteradores del vector, omite los repetidos
+    set<int> no_r_numbers(numbers.begin(), numbers.end());
+    cout << numbers.size() << endl;  // out: 8
+    cout << no_r_numbers.size() << endl;  // out: 5
--- Apuntes/exam6.cpp
+++ Apuntes/exam6.cpp
@@ -1,8 +1,8 @@
 #include <bits/stdc++.h>
-#include <type_traits>
+#include <algorithm>
 using namespace std;
 
-int main() {
+void sub_exam1() {
     // iterador random
     vector<int> v = {10,20,30,40};
     auto it1 = begin(v);
@@ -15,9 +15,34 @@ int main() {
 
     // distancia de dos posiciones
     auto dist = distance(begin(l), it2);
-    // Rvalue de avanzar o retroceder una posicion
+    // L-value de avanzar o retroceder una posicion
     auto nex = next(it2, 1);
     auto first = prev(it2, 3);
     // void para avanzar una posicion
     advance(it2, dist);
+}
+
+void sub_exam2() {
+    list<int> ls = {3,2,2,4,3,1,2,1,4,3,2,1};
+
+    // func. lambda:  parametros por defecto y SOLO un parametro referencial
+    auto five_div = [base= 5] (auto x) { return x % base = 0; };
+    /*  para funciones de return auto, los parametros deben ser L-values auto.  */
+
+    // contar elementos
+    cout << count(begin(ls), end(ls), 1) << endl;
+
+    int div = 2;
+    // contar elementos bajo una condicion con lambdas 
+    cout << count_if(begin(ls), end(ls), [] (int x) { return x % 2 == 0; }  );
+
+    // contar elementos bajo una condicion definida on parametros modificables
+    cout << count_if(begin(ls), end(ls), [&div] (int x) { return x % ++div == 0; }  );
+    /* Los parametros no locales pueden ser modificables si no se declara por defecto */
+}
+
+
+int main() {
+    sub_exam1();
+    sub_exam2();
 }
